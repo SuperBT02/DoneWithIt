a953bb075191d7badf06969139c1eb351ea51365
Rayen <rayenboubtane71@gmail.com>
2025-01-13 10:50:43 +0100

    Before refactoring

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import {
   View,
 } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
