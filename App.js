c4171c6d6753b553dd21a8a7a3b74eb5df842388
Rayen <rayenboubtane71@gmail.com>
2025-01-15 10:13:53 +0100

    reusable text styling component

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
-import {
-  View,
-} from "react-native";
+import { Text, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
+import AppText from "./app/components/AppText";
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AppText> I love React Native !</AppText>
+    </View>
+  );
 }
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
 
+import colors from "../config/colors";
+
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
       <View style={styles.closeIcon}></View>
-      <View style={styles.deleteIcon}></View>    
+      <View style={styles.deleteIcon}></View>
       <Image
         resizeMode="contain"
         style={styles.image}
@@ -18,27 +20,26 @@ const styles = StyleSheet.create({
   image: {
     height: "100%",
     width: "100%",
-    },
-    container: {
-        backgroundColor: '#000',
-        flex:'1',
-    },
-    closeIcon: {
-        width: 50,
-        height: 50,
-        backgroundColor: "#fc5c65",
-        position: "absolute",
-        top: 40,
-        left:30,
-    },
-    deleteIcon: {
-        width: 50,
-        height: 50,
-        backgroundColor: "#4ecdc4",
-        position: "absolute",
-        top: 40,
-        right:30,
-    },
-    
+  },
+  container: {
+    backgroundColor: colors.black,
+    flex: "1",
+  },
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.secondary,
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.primary,
+    position: "absolute",
+    top: 40,
+    right: 30,
+  },
 });
 export default ViewImageScreen;
