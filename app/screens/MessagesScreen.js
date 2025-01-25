f7f2156618b91fdbfc3169d0a9db3924aa5cd13a
Rayen <rayenboubtane71@gmail.com>
2025-01-25 22:25:54 +0100

    pull to refresh and screen size

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import ListItem from "../components/ListItem";
 import Screen from "../components/Screen";
@@ -23,6 +23,7 @@ const initialMessages = [
 
 function MessagesScreen(props) {
     const [messages, setMessages] = useState(initialMessages);
+    const [refreshing, setRefreshing] = useState(false);
     const handleDelete = message => {
         setMessages(messages.filter(m => m.id !== message.id));
     }
@@ -42,6 +43,17 @@ function MessagesScreen(props) {
                     />
                 )}
                 ItemSeparatorComponent={ListItemSeparator}
+                refreshing={refreshing}
+                onRefresh={() => {
+                    setMessages([
+                        {
+                            id: 2,
+                            title: "T2",
+                            description: "D2",
+                            image: require("../assets/mosh.jpg"),
+                        },
+                    ])
+                }}
             />
         </Screen>
     );
